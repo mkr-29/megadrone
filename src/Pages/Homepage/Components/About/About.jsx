71f777bf71e3f9dd52d73e9797cea3d5f4c81151
Mayank Kumar <mayank.k9802@gmail.com>
2023-08-21 22:41:01 +0530

    Update About.jsx

--- src/Pages/Homepage/Components/About/About.jsx
+++ src/Pages/Homepage/Components/About/About.jsx
@@ -14,14 +14,17 @@ export default function About() {
       <img src={RoboPoster} className="robo-poster" alt="" />
       <div className="about-text">
         <p id="upper-text">
-          Welcome to Robotrix: A 3-Day Workshop on Robotics and Robo Race!
+          Welcome to Megadrone: A 3-Day Workshop on Drone Making!
         </p>
         <p id="lower-text">
-          Are you fascinated by the world of robotics and eager to dive into its
-          intricate workings? Look no further! Join us for a thrilling and
-          immersive experience at Robotrix, a 3-day workshop that will introduce
-          you to the exciting realm of robotics and culminate in an
-          adrenaline-pumping robo race.
+          The MegaDrone workshop, organized by IEEE JUIT-SB, is a three-day
+          immersive event focused on teaching participants the art of assembling
+          FPV (First Person View) drones. Through hands-on sessions and expert
+          guidance, attendees will have the opportunity to learn the intricacies
+          of building their own drones from scratch. This workshop aims to
+          provide a comprehensive understanding of the various components
+          involved in drone construction, such as motors, flight controllers,
+          ESCs (Electronic Speed Controllers), frames, and FPV equipment.
         </p>
       </div>
     </div>
